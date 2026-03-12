f4f574215f03a85042ba07d805ac15fe105d11c3
Shrey Singh <9shrey@gmail.com>
2026-03-12 19:09:47 +0530

    Initial commit for J_Bday website

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,13 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  pc: "THIS IS THE THERAPY CORNER. WHERE YOU TALK TO GPT TO RESOLVE ALL YOUR ISSUES. YOU CAN TALK TO US FAM, I'LL GIVE WAY BETTER ADVICE THAN SOME STUPID CLANKER.",
+  "cs-degree": "THIS IS THE CARD I MADE YOU, YOU HAVE IT FRAMED ON THE WALL CAUSE IT'S SO GREAT. BEST GIFT EVER.",
+  "sofa-table": "THAT'S YOUR SOFA. THIS IS WHERE THE BARBIE DOLL INCIDENT OCCURED",
+  tv: "TV: REMEMBER WHEN WE ALL SAT AND WATCHED DONT BREATHE (YALL SHIT YOURSELF OUT OF FEAR). THE SAME TV WITNESSED THE BEST DANCE MOVES WHEN WE PLAYED WII. ALSO PEAK MARIO KART 4-WAY SPLIT SCREEN GAMES. RED SHELL INCOMING.",
+  bed: "THIS IS WHERE THE LADY BED ROTS. SCROLLING THROUGH REELS PRETENDING TO BE PRODUCTIVE.",
+  resume: "THANKS FOR BEING THE BESTEST FRIEND YOU LOSER",
+  projects: "GREEK RESTRAUNT IN THE MAKING RN. LET'S ALL BE RICH TOGETHER FR FR",
+  library: "YOU DONT READ, THIS IS JUST HERE FOR AESTHETICS. START READING SOME BOOKS OR YOULL BE STUPID FOREVER.",
+  exit: "DON'T LEAVE. (GET IT? DON'T BREATHE). HE HE HE. "
 };
--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
   sliceY: 31,
   anims: {
-    "idle-down": 936,
-    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
-    "idle-side": 975,
-    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
-    "idle-up": 1014,
-    "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+  // Female sprite: offset x by +16 columns
+  "idle-down": 936 + 16,
+  "walk-down": { from: 936 + 16, to: 939 + 16, loop: true, speed: 8 },
+  "idle-side": 975 + 16,
+  "walk-side": { from: 975 + 16, to: 978 + 16, loop: true, speed: 8 },
+  "idle-up": 1014 + 16,
+  "walk-up": { from: 1014 + 16, to: 1017 + 16, loop: true, speed: 8 },
   },
 });
 
